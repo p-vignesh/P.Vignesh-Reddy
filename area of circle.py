434aa1f709c1cabc3af490945eabda58d6808be1
p-vignesh <90907683+p-vignesh@users.noreply.github.com>
2022-06-13 22:17:50 +0530

    Update area of circle.py

--- area of circle.py
+++ area of circle.py
@@ -4,3 +4,8 @@ r = float(input('Enter the radius of the circle :'))
 area = math.pi * r * r
 
 print("Area of the circle is : %.16f" %area)
+
+filename = input("Enter Filename: ")
+f = filename.split(".")   #split-dividing the linento two
+
+print ("Extension of the file is : " + f[-1])
