ba0a58d9fa636a5b7d4c8e770fd956277176e04d
p-vignesh <90907683+p-vignesh@users.noreply.github.com>
2022-06-14 10:14:32 +0530

    Add files via upload

---
+++ comparison operators.py
@@ -0,0 +1,16 @@
+a=int(input("Enter the number : " ))
+b=int(input("Enter the number : " ))
+print(a)
+print(b)
+
+print(a>b)
+
+print(a<b)
+
+print(a<=b)
+
+print(a>=b)
+
+print(a==b)
+
+print(a!=b)
